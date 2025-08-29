97fc447ecee019c48fcd2ba73ddb89af3b8a4fca
Oswald <120295393+ox1nec@users.noreply.github.com>
2025-08-29 12:23:36 +0200

    fixes

--- utils/file_manager.py
+++ utils/file_manager.py
@@ -5,7 +5,7 @@ import os
 def load_from_json(path: str):
     if os.path.isfile(path):
         with open(path, encoding='utf-8') as file:
-            return json.load(file)
+            return json.load(file) 
     else:
         with open(path, 'x', encoding='utf-8') as file:
             example = {
